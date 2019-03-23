d3f23433fdd5e1eef3f114a6635cf1bc28454070
Nina <ninarachaebuie@gmail.com>
2019-03-22 21:39:52 -0500

    Updates

---
+++ precache-manifest.af006be9ac14204954241e9e9e1eb8f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f2107c166f965ae3f2026032f9e43e9",
+    "url": "/pokemon-mail/static/media/rusa.6f2107c1.ttf"
+  },
+  {
+    "revision": "831e1f4da157d96bc996f8c9f5f1e578",
+    "url": "/pokemon-mail/static/media/PokemonGB.831e1f4d.ttf"
+  },
+  {
+    "revision": "0a8b1950a48a4ee96ba53ac7963beb5f",
+    "url": "/pokemon-mail/static/media/PokemonDPPt.0a8b1950.ttf"
+  },
+  {
+    "revision": "7851a1a26d20c416c6cc0070a2bbcc3c",
+    "url": "/pokemon-mail/static/media/POKPIX3.7851a1a2.TTF"
+  },
+  {
+    "revision": "cab7d41da52431c52360e7e2f5c9d6e0",
+    "url": "/pokemon-mail/static/media/POKPIX1.cab7d41d.TTF"
+  },
+  {
+    "revision": "804f85a099944cd8fc2f",
+    "url": "/pokemon-mail/static/js/runtime~main.6e306020.js"
+  },
+  {
+    "revision": "abb1cca8820a79d7bcf4",
+    "url": "/pokemon-mail/static/js/main.597fea5e.chunk.js"
+  },
+  {
+    "revision": "ea583ad9cf72193b5a52",
+    "url": "/pokemon-mail/static/js/2.eed4770f.chunk.js"
+  },
+  {
+    "revision": "abb1cca8820a79d7bcf4",
+    "url": "/pokemon-mail/static/css/main.ff6fb505.chunk.css"
+  },
+  {
+    "revision": "b29f4d79b5d364f59431cd1f9621a14c",
+    "url": "/pokemon-mail/index.html"
+  }
+];
